4d9c449227609e72ceb8266c77d875e6965193ba
wsf <2313299730@.com>
2018-12-17 14:55:17 +0800

    "1.verson"

---
+++ src/main/java/com/baizhi/wsf/HouqiApplication.java
@@ -0,0 +1,14 @@
+package com.baizhi.wsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouqiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouqiApplication.class, args);
+    }
+
+}
+
